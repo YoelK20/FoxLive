120b2e92bf5841b28d86f430019fd5e5f9d3a15e
zakyabdurrahman <zakyabdurrahman10@gmail.com>
2024-07-16 18:15:07 +0700

    rip

--- client/src/Views/Streamer.jsx
+++ client/src/Views/Streamer.jsx
@@ -1,16 +1,62 @@
+import Peer from "peerjs";
 
 
 
 export default function StreamerPage() {
+    const peer = new Peer("streamer", {
+        host: "localhost",
+        port: 9000,
+        path: "/myapp",
+        config: {
+            'iceServers': [
+                { url: 'stun:stun1.l.google.com:19302' },
+                {
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'muazkh',
+                    username: 'webrtc@live.com'
+                }
+            ]
+        }
+    })
+
+    async function startStream() {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: true
+            });
+
+            const screen = document.getElementById('screen');
+            screen.srcObject = stream
+            
+            peer.on('open', (id) => {
+                console.log(id, 'peer id');
+                
+            })
+
+            peer.on('call', (call) => {
+                //answer call
+                console.log('answer');
+                call.answer(stream);
+            })
+
+            
+
+
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
 
     return (
         <>
             <div className="flex items-center h-screen w-full justify-center bg-white">
                 <div className="cols w-[700px] h-[500px] border border-2">
-                    <div className="h-[80%] w-full border border-2 mb-4">
-                    </div>
+                    <video autoPlay={true} id="screen" className="h-[100%] w-[100%] border border-2 mb-4">
+                    </video>
                     <div className="flex justify-center items-center">
-                        <button className="btn"> Stream Now</button>
+                        <button className="btn" onClick={() => startStream()}> Stream Now</button>
                     </div>
 
                 </div>
--- client/src/Views/Watcher.jsx
+++ client/src/Views/Watcher.jsx
@@ -1,17 +1,66 @@
+import Peer from "peerjs";
+import { useState } from "react";
 
 
 
 
 export default function WatcherPage(){
 
+    const peer = new Peer("watcher", {
+        host: "localhost",
+        port: 9000,
+        path: "/myapp",
+        config: {
+            'iceServers': [
+                { url: 'stun:stun1.l.google.com:19302' },
+                {
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'muazkh',
+                    username: 'webrtc@live.com'
+                }
+            ]
+        }
+    })
+    
+
+    async function startWatch(e) {
+        e.preventDefault();
+        try {
+            const peer = new Peer();
+        
+            const screen = document.getElementById('screen')
+            console.log(e.target[0].value)  ;
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: false
+            });
+            peer.connect("watcher");
+            peer.on('connection', (conn) => {
+                'connected'
+            })
+            const call = await peer.call("watcher", stream);
+            console.log(call);
+            call.on('stream', (stream) => {
+                screen.srcObj = stream
+                console.log('stream hit');
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return(
         <>
        <div className="flex items-center h-screen w-full justify-center bg-white">
                 <div className="cols w-[700px] h-[500px] border border-slate-500 border-2">
-                    <div className="h-[80%] w-full border border-black border-2 mb-4">
-                    </div>
+                    <video id="screen" autoPlay={true} className="h-[100%] w-full border border-black border-2 mb-4">
+                    </video>
                     <div className="flex justify-center items-center">
-                        <button className="btn">Watch Stream</button>
+                        <form onSubmit={startWatch}>
+                            <input type="text" name="" id="" />
+                            <button className="btn">Watch Stream</button>
+                        </form>
+                        
                     </div>
 
                 </div>
