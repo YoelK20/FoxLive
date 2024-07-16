b2838075c95554f9045b11713d70228b381173df
YoelK20 <166104713+YoelK20@users.noreply.github.com>
2024-07-16 15:46:49 +0700

    Waiting Client

--- client/src/Views/Homepage.jsx
+++ client/src/Views/Homepage.jsx
@@ -1,19 +1,14 @@
-
-
-
-
-
-export default function HomePage(){
-
-
-    return(
-
-        <>
-        <div className="flex items-center h-screen w-full justify-center bg-white"> 
-        <h1 className="">
-            Welcome to PuberTv
-        </h1>
+export default function HomePage() {
+  return (
+    <>
+      <div className="flex flex-row">
+        <div className="flex items-center h-screen w-full justify-center bg-white">
+          <h1 className="">Welcome to PuberTv</h1>
         </div>
-        </>
-    )
+        <div className="flex items-center ">
+          <h1>text</h1>
+        </div>
+      </div>
+    </>
+  );
 }
