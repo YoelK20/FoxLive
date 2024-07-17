b4e428a93180068134fec42e7a4d81b526186761
YoelK20 <166104713+YoelK20@users.noreply.github.com>
2024-07-17 14:12:18 +0700

    Login Register Done

--- client/src/Router/router.jsx
+++ client/src/Router/router.jsx
@@ -1,10 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomeLogin from "../Views/Login";
+import { createBrowserRouter, redirect } from "react-router-dom";
+import HomeLogin from "../Views/LoginPage";
 import BaseLayout from "../Views/BaseLayout";
 import StreamerPage from "../Views/Streamer";
 import WatcherPage from "../Views/Watcher";
 import HomePage from "../Views/Homepage";
 import { io } from "socket.io-client";
+import RegPage from "../Views/RegisterPage";
+import Toastify from "toastify-js"
+
 
 const socket = io("http://localhost:3000", {
     autoConnect:false
@@ -14,20 +17,69 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <HomeLogin />,
+    loader: () => {
+      if (localStorage.access_token) {
+        Toastify({
+          text: "You already logged in",
+          duration: 2000,
+          newWindow: true,
+          close: true,
+          gravity: "bottom",
+          position: "right",
+          stopOnFocus: true,
+          style: {
+            background: "#EF4C54",
+            color: "#17202A",
+            boxShadow: "0 5px 10px black",
+            fontWeight: "bold",
+          },
+        }).showToast();
+        return redirect("/");
+      }
+      return null;
+
+    }
   },
   {
+    path: "/register",
+    element: <RegPage />
+  },
+  { path: "/",
     element: <BaseLayout socket={socket} />,
+    loader: () => {
+      if (!localStorage.access_token) {
+        Toastify({
+          text: "Please log in first",
+          duration: 2000,
+          newWindow: true,
+          close: true,
+          gravity: "bottom",
+          position: "right",
+          stopOnFocus: true,
+          style: {
+            background: "#EF4C54",
+            color: "#17202A",
+            boxShadow: "0 5px 10px black",
+            fontWeight: "bold",
+          },
+        }).showToast();
+        return redirect("login");
+      }
+
+      return null;
+    },
     children: [
       {
-        path: "/",
+        index: true,
         element: <HomePage socket={socket} />,
+        
       },
       {
-        path: "/stream",
+        path: "stream",
         element: <StreamerPage />,
       },
       {
-        path: "/watch",
+        path: "watch",
         element: <WatcherPage />,
       },
     ],
