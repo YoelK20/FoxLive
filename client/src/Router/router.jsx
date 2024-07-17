7c75aa81de605e458eeec7c1561067b9b0a9f5c4
YoelK20 <166104713+YoelK20@users.noreply.github.com>
2024-07-17 14:13:22 +0700

    Udpate Reg

--- client/src/Router/router.jsx
+++ client/src/Router/router.jsx
@@ -42,7 +42,29 @@ const router = createBrowserRouter([
   },
   {
     path: "/register",
-    element: <RegPage />
+    element: <RegPage />,
+    loader: () => {
+      if (localStorage.access_token) {
+        Toastify({
+          text: "You already logged in",
+          duration: 2000,
+          newWindow: true,
+          close: true,
+          gravity: "bottom",
+          position: "right",
+          stopOnFocus: true,
+          style: {
+            background: "#EF4C54",
+            color: "#17202A",
+            boxShadow: "0 5px 10px black",
+            fontWeight: "bold",
+          },
+        }).showToast();
+        return redirect("/");
+      }
+      return null;
+
+    }
   },
   { path: "/",
     element: <BaseLayout socket={socket} />,
